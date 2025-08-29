e8eaceccf52fdb5c970cdc7a8031214f86755d01
RedLinesNT <redlinesnt@gmail.com>
2025-08-29 14:54:50 +0200

    Oops forgot the namespace impl

---
+++ Assets/Runtime/ConsoleCommand/NamespaceImpl.cs
@@ -0,0 +1,11 @@
+/*************************************************************************\
+Code provided under the "Apache Licence 2.0".
+For more details, refer to the "LICENCE" file at the root of the project.
+Written by RDNTLG (August 2025).
+\*************************************************************************/
+
+//Explicit declaration of the namespace "RDNTLG.Command" to
+//avoid having the compiler EXPLODE when not using
+//the "RDNTLG_CCM" def under certain conditions.
+
+namespace RDNTLG.Command {}
